b3e760b7b93d4d6f235eb43b4bfa55a9e742310d
KareemShaban1 <karim17196@fci.bu.edu.eg>
2023-10-15 07:06:42 +0300

    new updates 15/10/2023

---
+++ resources/lang/en/account_trans.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+          'User_Profile' => 'User Profile',
+          'Choose_Image' => 'Choose Image',
+          'Instagram_Url' => 'Instagram URL',
+          'Facebook_Url' => 'Facebook URL',
+          'Twitter_Url' => 'Twitter URL',
+          'LinkedIn_Url' => 'LinkedIn URL',
+          'Bio' => 'Bio',
+          'Update_Profile' => 'Update Profile',
+      ];
+
+      
